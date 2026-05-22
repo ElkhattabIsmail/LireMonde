891114f3d64aa7c2ac6a333c83add961a5f87fdc
ELKHATTAB SMAIL <elhattab207@gmail.com>
2026-05-22 05:28:44 +0100

    Render (books, ReadingList, Admin Table.

--- script.js
+++ script.js
@@ -77,8 +77,6 @@ async function saveBook(bookData, id = null) {
 
 }
 
-// ================= DELETE BOOK =================
-
 async function deleteBook(id) {
 
     const confirmed = confirm('Supprimer ce livre ?');
@@ -101,4 +99,159 @@ async function deleteBook(id) {
 
 }
 
+function renderBooks(filteredBooks = books) {
 
+    const container = document.getElementById('books-grid');
+
+    container.innerHTML = '';
+
+    filteredBooks.forEach(book => {
+
+        const card = document.createElement('div');
+
+        card.className = 'book-card';
+
+        card.innerHTML = `
+                    <img
+                        src="${book.couverture}"
+                        alt="${book.titre}"
+                        class="book-image"
+                    >
+
+                    <div class="book-info">
+
+                        <h3 class="book-title">
+                            ${book.titre}
+                        </h3>
+
+                        <p class="book-author">
+                            ${book.auteur}
+                        </p>
+
+                        <div class="book-footer">
+
+                            <span class="genre-badge">
+                                ${book.genre}
+                            </span>
+
+                            <button
+                                class="bookmark-btn ${book.aLire ? 'bookmark-active' : 'bookmark-inactive'}"
+                                onclick="event.stopPropagation(); toggleALire(${book.id})"
+                            >
+                                <i class="fa-solid fa-bookmark"></i>
+                            </button>
+
+                        </div>
+
+                    </div>
+                `;
+
+    });
+
+}
+
+
+function renderReadingList() {
+
+    const readingBooks = books.filter(book => book.aLire);
+
+    const container = document.getElementById('reading-grid');
+
+    container.innerHTML = '';
+
+    document
+        .getElementById('empty-reading')
+        .classList.toggle('hidden', readingBooks.length > 0);
+
+    readingBooks.forEach(book => {
+
+        const card = document.createElement('div');
+
+        card.className = 'book-card';
+
+        card.innerHTML = `
+                    <img
+                        src="${book.couverture}"
+                        alt="${book.titre}"
+                        class="book-image"
+                    >
+
+                    <div class="book-info">
+
+                        <h3 class="book-title">
+                            ${book.titre}
+                        </h3>
+
+                        <p class="book-author">
+                            ${book.auteur}
+                        </p>
+
+                        <button
+                            class="btn-remove"
+                            onclick="event.stopPropagation(); toggleALire(${book.id})"
+                        >
+                            Retirer
+                        </button>
+
+                    </div>
+                `;
+
+
+    });
+
+}
+
+function renderAdminTable() {
+
+    const tbody = document.getElementById('admin-table-body');
+
+    tbody.innerHTML = '';
+
+    books.forEach(book => {
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+                    <td>
+                        <img
+                            src="${book.couverture}"
+                            class="table-cover"
+                        >
+                    </td>
+
+                    <td>
+                        <strong>${book.titre}</strong>
+                    </td>
+
+                    <td>${book.auteur}</td>
+
+                    <td>
+                        <span class="genre-badge">
+                            ${book.genre}
+                        </span>
+                    </td>
+
+                    <td class="action-cell">
+
+                        <button
+                            class="icon-btn edit-btn"
+                            onclick="editBook(${book.id}); event.stopPropagation();"
+                        >
+                            <i class="fa-solid fa-pen"></i>
+                        </button>
+
+                        <button
+                            class="icon-btn delete-btn"
+                            onclick="deleteBook(${book.id}); event.stopPropagation();"
+                        >
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+
+                    </td>
+                `;
+
+        tbody.appendChild(row);
+
+    });
+
+}
